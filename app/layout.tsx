7ca196fed9352dfbdaea0475bd43ce79f47dc52b
nguyenkhanhhai63-bit <nguyenkhanhhai63@gmail.com>
2026-07-30 13:31:16 +0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./globals.css";
-export const metadata = { title: "Content Universe V6", description: "Siêu Di Động Content OS" };
-export default function RootLayout({children}:{children:React.ReactNode}) {
-  return <html lang="vi"><body>{children}</body></html>;
-}
